fb4f292a795d8caaffe264866e1d8bf2b5c5352d
JoaoDantas00 <joaodantas2000@yahoo.com>
2026-05-21 12:59:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME="passaro-rewards-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
