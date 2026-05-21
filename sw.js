ee4e94ca2b08c517e9fb56f8c88c6197a699c87a
JoaoDantas00 <joaodantas2000@yahoo.com>
2026-05-21 13:45:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="passaro-rewards-v2";
+const CACHE_NAME="passaro-rewards-v3";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim());});
